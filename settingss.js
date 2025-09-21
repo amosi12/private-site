f59873b0d88f31187a5510e56bc2f56a0e3b25db
amosi12 <220756773+amosi12@users.noreply.github.com>
2025-09-21 09:40:16 +0300

    Add files via upload

---
+++ settingss.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+  SESSION_ID: "",  // add your Session Id here
+  
+  OWNER_NUMBER: "255767862457", // put your phone number here
+  
+  PREFIX: ".", // prefix (e.g., ., /, !, *)
+  
+  TIMEZONE: "Africa/Dar es salam" //put your country timeZone....leave blank if u don't know.
+};
